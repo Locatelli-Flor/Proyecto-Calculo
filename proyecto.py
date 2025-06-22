bcf93becdce4cbc30a11b30dc3c0ea46d13561e0
Locatelli-Flor <floriana.locatelli@correo.ucu.edu.uy>
2025-06-22 18:05:24 -0300

    add parte 1 ejercicio 6

--- proyecto.py
+++ proyecto.py
@@ -19,8 +19,16 @@ def f3(r):
     return (G*M)/(24*r**5)
 
 
-def p1(r):
-    return f(RT) + f1(r) * (r - RT)
+def p1(r, a):
+    return f(a) + f1(r) * (r - a)
+
+
+def p2(r, a):
+    return p1(r, a) + f2(a) / 2 * (r - a)**2
+
+
+def p3(r, a):
+    return p2(r, a) + f3(a) / 6 * (r - a)**3
 
 
 print("--- Parte 1 ---")
@@ -32,3 +40,15 @@ h = 8849
 print(f"f(RT + h) = {f(RT + h):.4}")
 print(f"f(RT) = {f(RT):.4}")
 print(f"Diferencia relativa: {(f(RT + h) - f(RT))/f(RT):.2%}")
+
+
+print("- 6 -")
+
+print(f"f(0.01) = {f(0.01):.4}")
+print(f"f(0.02) = {f(0.02):.4}")
+print(f"Diferencia relativa: {(f(0.02) - f(0.01))/f(0.01):.2%}")
+
+print(f"p2(0.02) = {p2(0.02, 0.01):.4}")
+print(f"Diferencia relativa: {(p2(0.02, 0.01) - f(0.02))/f(0.02):.2%}")
+print(f"p3(0.02) = {p3(0.02, 0.01):.4}")
+print(f"Diferencia relativa: {(p3(0.02, 0.01) - f(0.02))/f(0.02):.2%}")
